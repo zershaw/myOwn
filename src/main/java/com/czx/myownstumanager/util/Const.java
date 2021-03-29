e4b223d3322ba02c70fc43ebfc078c84d0360e8e
zershaw <33977355+zershaw@users.noreply.github.com>
2021-03-29 21:15:33 +0800

    Update Const.java

--- src/main/java/com/czx/myownstumanager/util/Const.java
+++ src/main/java/com/czx/myownstumanager/util/Const.java
@@ -2,7 +2,7 @@ package com.czx.myownstumanager.util;
 
 public class Const {
     public static final String CODE = "code";
-    public static final String ADMIN = "admin";
+    public static final String ADMIN = "admin"; //标记一下
     public static final String STUDENT = "student";
     public static final String TEACHER = "teacher";
     public static final String USERTYPE = "usertype";
